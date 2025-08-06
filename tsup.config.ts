547a3bd80287ae4786e34ce6bf252e336c528ec2
maysara.elshewehy <maysara.elshewehy@gmail.com>
2025-08-07 02:17:32 +0300

    cleanup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,27 +1,10 @@
-// tsup.config.ts — Configuration for `tsup`.
-//
-// Developed with ❤️ by Maysara.
-
-
-
-// ╔════════════════════════════════════════ PACK ════════════════════════════════════════╗
-
-    import { defineConfig } from 'tsup';
-
-// ╚══════════════════════════════════════════════════════════════════════════════════════╝
-
-
-
-// ╔════════════════════════════════════════ PACK ════════════════════════════════════════╗
-
-    export default defineConfig
-    ({
-        entry                           : ["lib/lexer.ts"],
-        format                          : ["cjs", "esm"],
-        dts                             : true,
-        splitting                       : false,
-        sourcemap                       : true,
-        clean                           : true,
-    });
-
-// ╚══════════════════════════════════════════════════════════════════════════════════════╝
+import { defineConfig } from 'tsup';
+export default defineConfig
+({
+    entry                           : ["lib/lexer.ts"],
+    format                          : ["cjs", "esm"],
+    dts                             : true,
+    splitting                       : false,
+    sourcemap                       : true,
+    clean                           : true,
+});
